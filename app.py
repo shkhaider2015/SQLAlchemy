77aff9f5879d8fad966697ecab64b9f093ad616a
Shakeel Haider <43218437+shkhaider2015@users.noreply.github.com>
2021-02-14 14:51:25 +0500

    hgh

--- app.py
+++ app.py
@@ -1,1 +1,26 @@
-print("Hello")
+# from sqlalchemy import create_engine, Column, Integer, String, ForeignKey
+# from sqlalchemy.ext.declarative import declarative_base
+
+
+# base = declarative_base()
+
+# class Person(base):
+#     __tablename__ = "person"
+#     id = Column('id', Integer, primary_key=True)
+#     username = Column('username', String, unique=True)
+
+
+# engine = create_engine('sqlite:///:users.db', echo=True)
+# base.metadata.create_all(bind=engine)
+
+from sqlalchemy import create_engine, MetaData, Table, Column, Integer, String
+engine = create_engine('sqlite:///college.db', echo = True)
+meta = MetaData()
+
+students = Table(
+   'students', meta, 
+   Column('id', Integer, primary_key = True), 
+   Column('name', String), 
+   Column('lastname', String),
+)
+meta.create_all(engine)
